70b18016029547bc4786d924bc6bcae19b201840
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2023-09-19 15:15:07 -0300

    Exercícios JS

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,7 @@
-console.log('O console funcionou corretamnte')
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
